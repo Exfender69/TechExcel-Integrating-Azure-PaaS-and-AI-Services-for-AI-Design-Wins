e02d5230ca6f46c9ef3d565308a79e2de9cf63be
Exfender69 <53904273+Exfender69@users.noreply.github.com>
2025-04-16 09:45:49 +0000

    update 2

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,8 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+-- Create a user for the managed identity from the external provider
+CREATE USER [id-2s42gkxsoinrg] FROM EXTERNAL PROVIDER;
 
+-- Assign the user to the db_datareader role for read access
+ALTER ROLE db_datareader ADD MEMBER [id-2s42gkxsoinrg];
+
+-- Assign the user to the db_datawriter role for write access
+ALTER ROLE db_datawriter ADD MEMBER [id-2s42gkxsoinrg];
